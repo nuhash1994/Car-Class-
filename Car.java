b6a237dff247dfb394c203db8d50882d2233fe55
Nuhash Touhid <Nuhash Touhid>
2017-04-20 00:20:15 -0400

    initial commit

--- Car.java
+++ Car.java
@@ -4,67 +4,56 @@ public class Car {
 	private String color;
 	private int horsePower;
 	private double engine;
-	
-public Car(){
-	make="Honda";
-	color="Gray";
-	horsePower=350;
-	engine=3.5;
-}
-public Car(String make, String color, int horsePower, double engine){
-	this.make=make;
-	this.color=color;
-	this.horsePower=horsePower;
-	this.engine=engine;
-}
 
-public void setmake(String make){
-	this.make=make;
-}
+	public Car() {
+		make = "Honda";
+		color = "Gray";
+		horsePower = 350;
+		engine = 3.5;
+	}
 
-public void setcolor(String color){
-	this.color=color;
-}
-public void sethorsePower(int horsePower){
-	this.horsePower=horsePower;
-}
-public void setengine(double engine){
-	this.engine=engine;
-}
+	public Car(String make, String color, int horsePower, double engine) {
+		this.make = make;
+		this.color = color;
+		this.horsePower = horsePower;
+		this.engine = engine;
+	}
 
-public String getMake(){
-	return this.make;
-}
+	public void setmake(String make) {
+		this.make = make;
+	}
 
-public String getColor(){
-	return this.color;
-}
-	
-public int getHorsePower(){
-	return this.horsePower;
-}
+	public void setcolor(String color) {
+		this.color = color;
+	}
 
-public double getEngine(){
-	return this.engine;
-}
+	public void sethorsePower(int horsePower) {
+		this.horsePower = horsePower;
+	}
 
-public String toStirng(){
-	return "\nName: "+this.make+"\ncolor: "+this.color+"\nHorse Power: "+this.horsePower+"\nEngine Size: "+this.engine;
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public void setengine(double engine) {
+		this.engine = engine;
+	}
+
+	public String getMake() {
+		return this.make;
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+
+	public int getHorsePower() {
+		return this.horsePower;
+	}
+
+	public double getEngine() {
+		return this.engine;
+	}
+
+	public String toStirng() {
+		return "\nName: " + this.make + "\ncolor: " + this.color + "\nHorse Power: " + this.horsePower
+				+ "\nEngine Size: " + this.engine;
+	}
+
 }
